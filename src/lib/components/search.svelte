e677e6ef4a9c221a9687a4db422711567b515990
Castorri Lorenzo <lorenzo.castorri@studio.unibo.it>
2026-05-30 00:38:03 +0200

    Created a base for the 'champion' page.

---
+++ src/lib/components/search.svelte
@@ -0,0 +1,24 @@
+<script>
+
+</script>
+
+<div class="search-container" id="head-section">
+    <input type="text" class="search" placeholder="Search...">
+    <div class="filler"></div>
+    <div class="filter" id="filter">Filter</div>
+</div>
+
+<style>
+    .search-container{
+        display: flex;
+        flex-direction: row;
+    }
+
+    .search{
+
+    }
+
+    .filter{
+        
+    }
+</style>
--- src/routes/champions/+page.svelte
+++ src/routes/champions/+page.svelte
@@ -1,6 +1,47 @@
 <script>
   import Navbar from "$lib/components/navbar.svelte";
+  import Search from "$lib/components/search.svelte";
 </script>
 
-<Navbar />
-<div class="builder-container">champions!!</div>
+<div class="champion-container">
+  <div class="champion-content">
+    <Navbar />
+    <div class="title">Champion</div>
+    <Search />
+    <div class="grid-container">
+      {#each ["akali", "aurora", "b", "d", "supercalifragilistichespiralidoso", "z"] as champion}
+      <div class="element">
+        <div class="champion">{champion}</div>
+      </div>
+      {/each}
+    </div>
+  </div>
+</div>
+
+<style>
+  .champion-content{
+    display: flex;
+    flex-direction: column;
+  }
+
+  .grid-container{
+    display: grid;
+    justify-content: center;
+    align-items: center;
+    grid-template-columns: repeat(5, auto);
+    column-gap: clamp(.5rem, 3vw, 3rem);
+    row-gap: 10px;
+    padding-left: 5%;
+    padding-right: 5%;
+    background-color: aqua;
+  }
+
+  .element{
+    display: flex;
+  }
+
+  .champion{
+    flex-grow: 1;
+    background-color: blue;
+  }
+</style>

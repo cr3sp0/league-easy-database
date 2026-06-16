ed14286a7650226f6117f5e22732de0e3de7ea1f
Castorri Lorenzo <lorenzo.castorri@studio.unibo.it>
2026-06-16 11:10:17 +0200

    Added basic login: it just redirects to a template account without checking its existence.

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,6 +1,15 @@
-<script>
+<script lang="ts">
   import Navbar from "$lib/components/navbar.svelte";
+  import { redirect } from "@sveltejs/kit";
+
+  let user: String = $state("");
 </script>
 
 <Navbar />
 <div class="builder-container">login!!</div>
+<div>
+  <div role="button">
+    <input type="text" bind:value={user} />
+    <a href="/account/{user}" onkeydown={(e) => e.key === "Enter"}>login</a>
+  </div>
+</div>

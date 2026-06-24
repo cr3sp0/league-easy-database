c57cc8bdb15a30117a3523c327f38fce32abf4db
Alessio Crespolini <crespolini.alessio@gmail.com>
2026-06-24 21:26:29 +0200

    Error 500 fix

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,7 +3,6 @@ import prisma from '$lib/server/prisma';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ params, cookies }) => {
-  const users = await prisma.user.findMany();
 
   return {
     profile: cookies.get('ledb_session')

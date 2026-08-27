7e3b3f4e665528cb45009da776e1612bcc53cc3a
Alessio Crespolini <crespolini.alessio@gmail.com>
2026-08-27 17:19:55 +0200

    Small animation between pages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,27 @@
 <script lang="ts">
+  import { onNavigate } from "$app/navigation";
   import favicon from "$lib/assets/favicon.svg";
   import Popup from "$lib/components/popup.svelte";
   import { popup } from "$lib/components/store/popup.svelte";
   import "../app.css";
 
   let { children } = $props();
+
+  onNavigate((navigation) => {
+    if (!document.startViewTransition) return;
+
+    return new Promise((resolve) => {
+      document.startViewTransition(async () => {
+        resolve();
+        await navigation.complete;
+      });
+    });
+  });
 </script>
 
 <svelte:head>
   <link rel="icon" href={favicon} />
 </svelte:head>
 
-<Popup color={popup.color} bind:text={popup.text}/>
+<Popup color={popup.color} bind:text={popup.text} />
 {@render children()}

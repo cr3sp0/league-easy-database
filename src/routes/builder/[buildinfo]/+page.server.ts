d26dacfac0f393d7d66e38d083016559ae7372ba
Alessio Crespolini <crespolini.alessio@gmail.com>
2026-08-31 16:15:56 +0200

    buildinfo upgrade

--- src/routes/builder/[buildinfo]/+page.server.ts
+++ src/routes/builder/[buildinfo]/+page.server.ts
@@ -1,12 +1,23 @@
+import { getChampionAndCosmeticsByName, getChampionByName } from '$lib/server/championsManager';
 import prisma from '$lib/server/prisma';
+import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({params, cookies}) => {
   const items = await prisma.oggetto.findMany();
 
+  let champname = params.buildinfo.split("-")
+
+  const champ = await getChampionByName(champname[0]);
+
+  if (!champ) {
+        throw error(404, 'Campione non trovato'); 
+    }
+
   return {
     profile: cookies.get('ledb_session'),
-    champ: params.buildinfo,
-    items: items
+    title: params.buildinfo,
+    items: items,
+    champ: champ
   };
 };

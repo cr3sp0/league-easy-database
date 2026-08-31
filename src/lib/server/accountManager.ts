c7198e5ef52622a71a7147ed98e6e0872b9ef461
Castorri Lorenzo <lorenzo.castorri@studio.unibo.it>
2026-08-31 21:56:10 +0200

    Declared functions handling the queries for the Account.

---
+++ src/lib/server/accountManager.ts
@@ -0,0 +1,33 @@
+export async function login() {
+    
+}
+export async function createSessione() {
+    
+}
+export async function deleteSessione() {
+    
+}
+export async function logout() {
+    
+}
+export async function registration() {
+    
+}
+export async function getAccount() {
+
+}
+export async function updateAccount() {
+
+}
+export async function getReport() {
+    
+}
+export async function createReport() {
+    
+}
+export async function deleteReport() {
+    
+}
+export async function banAccount() {
+    
+}

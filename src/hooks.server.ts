8e6e2d8ce7a3a1fded9a0b21f6da4b3c3b627399
Castorri Lorenzo <lorenzo.castorri@studio.unibo.it>
2026-09-02 18:05:50 +0200

    Organized queries reguarding the Account and the Session its appropriate manager.

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,27 +1,17 @@
 // hooks.server.ts
 
-import prisma from '$lib/server/prisma';
+import { getActiveSession } from '$lib/server/accountManager';
 import { redirect, type Handle } from '@sveltejs/kit'; 
 
 export const handle: Handle = async ({ event, resolve }) => {
 
-    const sessionGUID = event.cookies.get('ledb_session')
-    if(sessionGUID) {
-        let dbSession = await prisma.sessione.findFirst({
-            where: {
-                AND: [
-                    { Id: parseInt(sessionGUID) },
-                    { date_expired: { gt: new Date(Date.now()) } }
-                ]
-            },
-            include: {
-                user: true
-            }
-        })
+    const sessionID = event.cookies.get('ledb_session')
+    if(sessionID) {
+        let dbSession = await getActiveSession({ sessionID: parseInt(sessionID) })
 
         if(dbSession && dbSession.user) {
             event.locals.user = {
-                userID: dbSession.user_id,
+                userID: dbSession.session.user_id,
                 username: dbSession.user.Nome,
                 pfp: dbSession.user.Immagine,
                 isAdmin: dbSession.user.IsAdmin,
